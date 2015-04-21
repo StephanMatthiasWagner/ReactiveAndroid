7ee8116ac4753a0a04d8c1b743be268e1f46f39d
Stephan Wagner <stephan.wagner.mi738@gmail.com>
2015-04-21 19:45:20 +0200

    clearing rx data

--- RXConcurrency/src/main/java/com/wagner/android/MainActivity.java
+++ RXConcurrency/src/main/java/com/wagner/android/MainActivity.java
@@ -20,8 +20,7 @@ import rx.schedulers.Schedulers;
  * Time: 19:35
  * To change this template use File | Settings | File Templates.
  */
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     private static final String SAVED_INSTANCE_SOME_KEY = "SOME_KEY";
@@ -34,32 +33,33 @@ public class MainActivity extends Activity
     /**
      * The subscriber.
      */
-    private Subscriber<String> mySubscriber;
+    private Subscriber<String> firstSubscriber;
+    private Subscriber<String> secondSubscriber;
 
     /**
      * The Constructor
      */
-    public MainActivity()
-    {
+    public MainActivity() {
         Log.d(TAG, "call constructor");
     }
 
     private TextView firstObserverOutput;
+    private TextView secondObserverOutput;
     private Subscription subscription;
+
     /**
      * Called when the activity is first created.
+     *
      * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this
-     * Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is
-     * null.</b>
+     *                           Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is
+     *                           null.</b>
      */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "ACTIVITY JUST CREATED");
         super.onCreate(savedInstanceState);
 
-        if(savedInstanceState!=null && savedInstanceState.containsKey(SAVED_INSTANCE_SOME_KEY))
-        {
+        if (savedInstanceState != null && savedInstanceState.containsKey(SAVED_INSTANCE_SOME_KEY)) {
             savedInstance = savedInstanceState.getString(SAVED_INSTANCE_SOME_KEY);
         }
 
@@ -68,55 +68,67 @@ public class MainActivity extends Activity
         //create view with different fields
         //create button for starting with each field
 
-        firstObserverOutput = (TextView)findViewById(R.id.firstObserverOutput);
-        firstObserverOutput.setText("blablub");
+        firstObserverOutput = (TextView) findViewById(R.id.firstObserverOutput);
+        firstObserverOutput.setText("This is the output of first observer:\n");
 
+        secondObserverOutput = (TextView) findViewById(R.id.secondObserverOutput);
+        secondObserverOutput.setText("This is the output of second observer:\n");
         //initSubscription();
 
     }
 
 
     @Override
-    public void onDestroy()
-    {
-        if(subscription!=null)
-        {
+    public void onDestroy() {
+        if (subscription != null) {
             subscription.unsubscribe();
         }
     }
 
-    public void initSubscription(View aView){
-        mySubscriber = getSubscriber();
+    public void initSubscription(View aView) {
+
+        if (aView.getId() == R.id.startSubscription1) {
+            firstSubscriber = getFirstSubscriber();
+
+            subscription = getSubscription(firstSubscriber);
+
+        }
+        if (aView.getId() == R.id.startSubscription2) {
+            secondSubscriber = getSecondSubscriber();
+
+            subscription = getSubscription(secondSubscriber);
+
+        }
+    }
+
+    private Subscription getSubscription(Subscriber<String> aSubscriber) {
         RandomPrimeNumGenerator randomPrimeNumGenerator = new RandomPrimeNumGenerator();
 
-        subscription = randomPrimeNumGenerator.getObservable().observeOn(AndroidSchedulers.mainThread())
+        return randomPrimeNumGenerator.getObservable().observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.newThread())
                 .filter(new Func1<String, Boolean>() {
                     @Override
                     public Boolean call(String item) {
-                        try{
+                        try {
                             Integer.valueOf(item);
-                        }catch (NumberFormatException e)
-                        {
+                        } catch (NumberFormatException e) {
                             return false;
                         }
                         return true;
                     }
                 }
                 )
-                .subscribe(mySubscriber);
-
+                .subscribe(aSubscriber);
     }
 
-
-    private Subscriber<String> getSubscriber(){
+    private Subscriber<String> getFirstSubscriber() {
 
         return new Subscriber<String>() {
             @Override
             public void onNext(String s) {
                 //Actualize the view + setting value
                 String lastOutput = firstObserverOutput.getText().toString();
-                firstObserverOutput.setText(lastOutput + " \n " +s);
+                firstObserverOutput.setText(lastOutput + " \n " + s);
 
                 firstObserverOutput.invalidate();
             }
@@ -127,11 +139,45 @@ public class MainActivity extends Activity
             }
 
             @Override
-            public void onError(Throwable e)
-            {
+            public void onError(Throwable e) {
                 //show in view that observer ran in an error
             }
         };
     }
 
+    private Subscriber<String> getSecondSubscriber() {
+
+        return new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                //Actualize the view + setting value
+                String lastOutput = secondObserverOutput.getText().toString();
+                secondObserverOutput.setText(lastOutput + " \n " + s);
+
+                secondObserverOutput.invalidate();
+            }
+
+            @Override
+            public void onCompleted() {
+                //show in view that observer is ready
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                //show in view that observer ran in an error
+            }
+        };
+    }
+
+
+    public void clearOutput(View aView) {
+        if (aView.getId() == R.id.clearOutput1) {
+            firstObserverOutput.setText("");
+            firstObserverOutput.invalidate();
+        }
+        if (aView.getId() == R.id.clearOutput2) {
+            secondObserverOutput.setText("");
+            secondObserverOutput.invalidate();
+        }
+    }
 }
